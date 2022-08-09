4b7d2b6470f7f5bff1d5a3fbb3cbbc66c06d6db1
liuming <liuming_it@sina.com>
2022-08-09 22:00:47 +0800

    1

--- store/src/main/java/com/liuming/store/StockApplication.java
+++ store/src/main/java/com/liuming/store/StockApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(com.liuming.stock.StockApplication.class, args);
+        SpringApplication.run(StockApplication.class, args);
     }
 }
